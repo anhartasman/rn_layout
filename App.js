996ad268ee54c61298b70e11a51a5dd94dead4e4
Anhar <anhar_tasman@yahoo.com>
2024-04-12 09:00:35 +0700

    Children and Container

--- App.js
+++ App.js
@@ -4,7 +4,55 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box></Box>
+      <Box
+        style={{
+          backgroundColor: "#8e9b00",
+        }}
+      >
+        Box 1
+      </Box>
+      <Box
+        style={{
+          backgroundColor: "#b65d1f",
+        }}
+      >
+        Box 2
+      </Box>
+      <Box
+        style={{
+          backgroundColor: "#1c4c56",
+        }}
+      >
+        Box 3
+      </Box>
+      <Box
+        style={{
+          backgroundColor: "#ab9156",
+        }}
+      >
+        Box 4
+      </Box>
+      <Box
+        style={{
+          backgroundColor: "#6b0803",
+        }}
+      >
+        Box 5
+      </Box>
+      <Box
+        style={{
+          backgroundColor: "#1c4c56",
+        }}
+      >
+        Box 6
+      </Box>
+      <Box
+        style={{
+          backgroundColor: "#b95f21",
+        }}
+      >
+        Box 7
+      </Box>
     </View>
   );
 }
@@ -12,5 +60,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
   },
 });
